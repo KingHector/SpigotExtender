5aab1e097ddfde2a91cb46b3a144b6f487f0264c
Hector Angelopoulos <62361708+KingHector@users.noreply.github.com>
2022-05-29 23:02:34 +0300

    Integrated Maven, Created BlockTouch

---
+++ src/main/java/com/king_hector/SpigotExtender/SpigotExtender.java
@@ -0,0 +1,14 @@
+package com.king_hector.SpigotExtender;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class SpigotExtender extends JavaPlugin
+{
+
+    public void onEnable()
+    {
+
+    }
+
+}
