ffd4a45cf78b88acd56ced3e12bc9d634f1b52ae
Hector Angelopoulos <62361708+KingHector@users.noreply.github.com>
2022-06-01 17:21:40 +0300

    Added Dependencies. Minor Fixes.

--- src/main/java/com/king_hector/SpigotExtender/SpigotExtender.java
+++ src/main/java/com/king_hector/SpigotExtender/SpigotExtender.java
@@ -3,9 +3,9 @@ package com.king_hector.SpigotExtender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
-public class SpigotExtender extends JavaPlugin
+public final class SpigotExtender extends JavaPlugin
 {
-
+    @Override
     public void onEnable()
     {
 
